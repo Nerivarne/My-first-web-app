162722d149d4de16eef5a7e7e99025d4ae681cd5
Nerivarne <sandrakubenova@gmail.com>
2022-01-05 17:51:29 +0100

    opravila jsem objekty

--- scripts.js
+++ scripts.js
@@ -1,39 +1,39 @@
-let bengalska {
+let bengalska = {
     photo: "/images/bengalska-kocka.jpg",
     title: "Bengálská kočka",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
 }
-let birma {
+let birma = {
     photo: "/images/birma.jpg",
     title: "Birma",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
 }
-let britka {
+let britka = {
     photo: "/images/britska-kocka.jpg",
     title: "Britská kočka",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
 }
-let habesska {
+let habesska = {
     photo: "/images/habesska-kocka.jpg",
     title: "Habešská kočka",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
 }
-let mainka {
+let mainka = {
     photo: "/images/mainska-myvali-kocka.jpg",
     title: "Mainská mývalí kočka",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
 }
-let ragdoll {
+let ragdoll = {
     photo: "/images/ragdoll.jpg",
     title: "Ragdoll",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
 }
-let siamka {
+let siamka = {
     photo: "/images/siamska-kocka.jpg",
     title: "Siamská kočka",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
 }
-let sphynx {
+let sphynx = {
     photo: "/sphynx.jpg",
     title: "Sphynx",
     description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
