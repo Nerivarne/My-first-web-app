c684432da7d12e5793a2d73f226e606abaf478df
Nerivarne <sandrakubenova@gmail.com>
2022-01-05 17:49:59 +0100

    Add objects

---
+++ scripts.js
@@ -0,0 +1,40 @@
+let bengalska {
+    photo: "/images/bengalska-kocka.jpg",
+    title: "Bengálská kočka",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
+let birma {
+    photo: "/images/birma.jpg",
+    title: "Birma",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
+let britka {
+    photo: "/images/britska-kocka.jpg",
+    title: "Britská kočka",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
+let habesska {
+    photo: "/images/habesska-kocka.jpg",
+    title: "Habešská kočka",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
+let mainka {
+    photo: "/images/mainska-myvali-kocka.jpg",
+    title: "Mainská mývalí kočka",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
+let ragdoll {
+    photo: "/images/ragdoll.jpg",
+    title: "Ragdoll",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
+let siamka {
+    photo: "/images/siamska-kocka.jpg",
+    title: "Siamská kočka",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
+let sphynx {
+    photo: "/sphynx.jpg",
+    title: "Sphynx",
+    description: "Sem musím pak vložit nějaké info o daném plemeni kočky."
+}
